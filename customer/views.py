bc663a7bef03e654555c9ca833245426e9c34535
CodewithAndare-hub <139978681+CodewithAndare-hub@users.noreply.github.com>
2024-07-15 03:29:14 +0300

    Update views.py

--- customer/views.py
+++ customer/views.py
@@ -124,11 +124,12 @@ class OrderConfirmation(View):
     def post(self, request, pk):
         order = OrderModel.objects.get(pk=pk)
         phone_number = request.POST.get('formId1')
+        number='{phone_number}'
         total = int(order.price)  
         # sending the prompt to the user
         cl = MpesaClient()
         # Use a Safaricom phone number that you have access to, for you to be able to view the prompt.
-        phone_number = phone_number
+        phone_number = number
         amount = total
         account_reference = 'reference'
         transaction_desc = 'Description'
